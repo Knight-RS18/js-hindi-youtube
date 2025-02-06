2da9605f334f51e63629b4593f47cbbefa951d5f
Suchintan Roy <153556710+Knight-RS18@users.noreply.github.com>
2025-02-06 15:41:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = true;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33" => NaN(not a number)
+// true => 1; false => 0;
+// null => 0;
+// undefined => NaN
+
+
+let isLoggedIn = "hitesh";
+
+let booleanIsloggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsloggedIn);
+
+
+// 1 => true; o => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+

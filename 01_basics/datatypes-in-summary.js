0dca3f1ec879825abc46ba36ba739fded6f4677d
Suchintan Roy <153556710+Knight-RS18@users.noreply.github.com>
2025-02-07 17:56:44 +0000

    stack and heap memory

--- 01_basics/datatypes-in-summary.js
+++ 01_basics/datatypes-in-summary.js
@@ -33,4 +33,31 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++
+
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);

d7df55abb896d048245c792c6a9f5a3a3eba2a5f
Suchintan Roy <153556710+Knight-RS18@users.noreply.github.com>
2025-02-08 09:20:15 +0000

    api talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -50,13 +50,111 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
 
 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor : instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+/////++++++++ some api talk +++++++++++///////
+
+
+// {
+//     "name": "hitesh",
+//    " coursename": "js in hindi",
+//     "price": 'free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
+
+// https://randomuser.me/api/
+
+// {
+//     "results": [
+//       {
+//         "gender": "male",
+//         "name": {
+//           "title": "Mr",
+//           "first": "Roy",
+//           "last": "Kunze"
+//         },
+//         "location": {
+//           "street": {
+//             "number": 4695,
+//             "name": "Grüner Weg"
+//           },
+//           "city": "Weinsberg",
+//           "state": "Brandenburg",
+//           "country": "Germany",
+//           "postcode": 53385,
+//           "coordinates": {
+//             "latitude": "-52.3745",
+//             "longitude": "-103.5952"
+//           },
+//           "timezone": {
+//             "offset": "+11:00",
+//             "description": "Magadan, Solomon Islands, New Caledonia"
+//           }
+//         },
+//         "email": "roy.kunze@example.com",
+//         "login": {
+//           "uuid": "d7f76e37-74ba-459a-82bd-3eb248eecbe2",
+//           "username": "sadsnake277",
+//           "password": "brownie",
+//           "salt": "ErTOxKNG",
+//           "md5": "ecafe0547ae8ab66bba1013304d8305e",
+//           "sha1": "5efd8ff1af97496810c64da69b6630cc0ab80257",
+//           "sha256": "3f91d2e7728194ec384047859253080021a7215178b975d1079d7dcc31f4ce2d"
+//         },
+//         "dob": {
+//           "date": "1994-07-09T03:24:53.995Z",
+//           "age": 30
+//         },
+//         "registered": {
+//           "date": "2016-10-24T01:54:12.332Z",
+//           "age": 8
+//         },
+//         "phone": "0358-9779170",
+//         "cell": "0176-8824068",
+//         "id": {
+//           "name": "SVNR",
+//           "value": "15 080794 K 473"
+//         },
+//         "picture": {
+//           "large": "https://randomuser.me/api/portraits/men/14.jpg",
+//           "medium": "https://randomuser.me/api/portraits/med/men/14.jpg",
+//           "thumbnail": "https://randomuser.me/api/portraits/thumb/men/14.jpg"
+//         },
+//         "nat": "DE"
+//       }
+//     ],
+//     "info": {
+//       "seed": "fac13747e993d938",
+//       "results": 1,
+//       "page": 1,
+//       "version": "1.4"
+//     }
+//   }

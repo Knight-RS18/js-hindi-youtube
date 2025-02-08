105e868505072ad5fc6e9c8f26dc4ccbf379f4ce
Suchintan Roy <153556710+Knight-RS18@users.noreply.github.com>
2025-02-08 14:03:24 +0000

    Arrow and this keyword

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,57 @@
+const user = {
+    username : "hitesh",
+    price : 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai();/
+
+
+// const chai = function(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// const chai =() => {
+//     let username = "hitesh"
+//     console.log(this);
+// }
+
+// chai()
+
+//basic arrow function =>
+ 
+// Explicit return =>
+// const addtwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// Implicit return =>
+// const addtwo = (num1, num2) => num1 + num2
+
+
+// const addtwo = (num1, num2) => (num1 + num2)
+
+const addtwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addtwo(3, 4))
+
+const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
